14b8d8ea43169e15ad414c2acf96e8e2d43fa902
PaulHowell <keikun12@gmail.com>
2019-10-20 17:32:14 +0900

    コメント付け加え

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,6 @@ const express = require('express');
 const basicAuth = require('./auth');
 const app = express();
 
-app.all('/stalls/:stallId/(*)', basicAuth); //正規表現の*が()内でしかうまく解釈されないのはexpress4系の仕様。WARNING 5系で動かなくなるかも
+app.all('/stalls/:stallId/(*)', basicAuth); //正規表現の*が()内でしかうまく解釈されないのはexpress4系の仕様。WARNING express5系で動かなくなるかも
 app.use(express.static(__dirname + '/static/'));
 exports.app = functions.https.onRequest(app);
